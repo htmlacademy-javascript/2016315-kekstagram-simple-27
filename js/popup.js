25c5eed5254b4072c1ea069a8d95c84702699884
Юрий Сажнев <adrenalin7828@yandex.ru>
2022-11-02 23:54:35 +0500

    Исправил все работает

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,31 @@
-import {InfoPictures} from './data.js';
+import {infoPictures} from './data.js';
 
-//const imgPopup = document.querySelector('.img-upload__overlay');
-//imgPopup.classList.remove('hidden');
+const formPictures = document.querySelector('.pictures');//Находим html элемент (div или что там), куда мы будем вставлять созданные узлы
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');//Находим шаблон узла.Обращается к свойству .content шаблона.Внутри свойства content дополнительно ищем тот узел, который будем копировать
 
-const pictureTemplate = document.querySelector('#picture').content;
+const similarPictures = infoPictures();
 
-const FormPictures = document.querySelector('.pictures').content;
-
-const similarPictures = InfoPictures();
-
-const similarListFragment = document.createDocumentFragment();
+const similarListFragment = document.createDocumentFragment();//Делаем фрагмент (если нужно), который будет служить эдаким буфером, куда мы будем собирать узел.
 
 similarPictures.forEach(({url, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
+  const pictureElement = pictureTemplate.cloneNode(true);//Делаем клон узла из шаблона
+  //Набиваем клон содержимым
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments;
-  similarListFragment.appendChild(pictureTemplate);
+  similarListFragment.append(pictureElement);//Запихиваем клон в фрагмент
 });
 
-FormPictures.appendChild(similarListFragment);
+formPictures.append(similarListFragment);//Запихиваем фрагмент в родитель
+
+//Расписываю по шагам просто на русском:
+//1. Находим html элемент (div или что там), куда мы будем вставлять созданные узлы
+//2. Находим шаблон узла.
+//2.1 Обращается к свойству .content шаблона
+//2.2 Внутри свойства content дополнительно ищем тот узел, который будем копировать
+//3. Делаем фрагмент (если нужно), который будет служить эдаким буфером, куда мы будем собирать узел.
+//4. Делаем клон узла из пункта 2.2
+//5. Набиваем клон содержимым
+//6. Запихиваем клон в фрагмент (пункт 3)
+//7. Запихиваем фрагмент в родитель (пункт 1).
+//Как только мы запихнули фрагмент, он тут же стал пустым и ждёт следующих поступлений.
